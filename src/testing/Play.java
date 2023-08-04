dd4facae75eafc54207e2636df625796e8eb7d4c
Tenno641 <ahmedfotoh2003@gmail.com>
2023-08-04 12:02:22 +0300

    LamMmMmMmbdDdDdDda N' ThreaAaAaAaAaAads.

--- src/testing/Play.java
+++ src/testing/Play.java
@@ -1,24 +1,58 @@
 package testing;
 
 import java.lang.Exception;
+import java.util.ArrayDeque;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
+
 
 public class Play {
 
     public static void main(String[] args) {
 
-        Queue<String> queue = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
-         
+        System.out.println(solve(input));
 
+    }
 
+    public static boolean solve(String input) {
+
+        String opening = "({[";
+        String closing = ")}]";
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char current = input.charAt(i);
+
+            if (opening.contains(String.valueOf(current))) {
+                deque.push(current);
+            } else {
+                try {
+                    char last = deque.pop();
+
+                    if (opening.indexOf(last) != closing.indexOf(current)) {
+                        return false;
+                    }
+
+                } catch (NoSuchElementException e) {
+                    return false;
+                }
+
+            }
+        }
+
+        return deque.isEmpty();
 
     }
 
 }
+
 
 class myException extends Exception{
 

31c4034de5a2f97192c159d86005966173d96d7e
Tenno641 <ahmedfotoh2003@gmail.com>
2023-08-08 10:37:25 +0300

    Fucktory method Pattern.

--- src/patterns/Factory/Factory.java
+++ src/patterns/Factory/Factory.java
@@ -4,112 +4,92 @@ public class Factory {
 
     public static void main(String[] args) {
 
-        System.out.println(500000 % 60);
+
 
     }
 
 }
 
-class SchoolStaticFactory {
+class BirdsFactory {
 
-    public static School newInstance(String type) {
-
-        return switch (type) {
-            case "school" -> new School();
-
-            case "subject" -> new Subjects();
-
-            case "student" -> new Students();
-
-            default -> throw new IllegalStateException("Unexpected value: " + type);
+    static Bird getInstance(String choice) {
+        return switch (choice) {
+            case "Duck" -> new Duck();
+            case "Chicken" -> new Chicken();
+            case "Pigeon" -> new Pigeon();
+            default -> throw new IllegalStateException();
         };
-
-    }
-}
-
-class School {
-
-    private String name;
-    private String address;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
 }
 
-class Subjects extends School {
+abstract class Bird{
 
-    private String name;
-    private int fullMark;
+    abstract void eat();
 
-    public String getName() {
-        return name;
-    }
+    abstract void makeSound();
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    abstract void makeEggs();
 
-    public int getFullMark() {
-        return fullMark;
-    }
-
-    public void setFullMark(int fullMark) {
-        this.fullMark = fullMark;
-    }
 }
 
-class Students extends School {
+class Duck extends Bird {
 
-    private String name;
-    private int studentClass;
-    private int age;
-    private int grade;
+    String name = "Duck";
 
-    public String getName() {
-        return name;
+    @Override
+    void eat() {
+        System.out.println("Duck eating");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    void makeEggs() {
+        System.out.println("Golden Egg");
     }
 
-    public int getStudentClass() {
-        return studentClass;
+    @Override
+    void makeSound() {
+        System.out.println("Quack");
     }
 
-    public void setStudentClass(int studentClass) {
-        this.studentClass = studentClass;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getGrade() {
-        return grade;
-    }
-
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
 }
 
+class Chicken extends Bird {
 
+    String name = "Chicken";
+
+    @Override
+    void eat() {
+        System.out.println("Chicken eating");
+    }
+
+    @Override
+    void makeEggs() {
+        System.out.println("Silver Egg");
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("idk.");
+    }
+
+}
+
+class Pigeon extends Bird {
+
+    @Override
+    void eat() {
+        System.out.println("Duck eating");
+    }
+
+    @Override
+    void makeEggs() {
+        System.out.println("Cutie Egg");
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("CLCKCKCLCKCK");
+    }
+
+}

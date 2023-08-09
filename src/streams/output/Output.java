52489b76152b6a971b8c0cc1413caf826cbf7d9b
Tenno641 <ahmedfotoh2003@gmail.com>
2023-08-09 17:40:07 +0300

    OutputSTREAMSSSSSS

--- src/streams/output/Output.java
+++ src/streams/output/Output.java
@@ -7,11 +7,13 @@ public class Output {
 
     public static void main(String[] args) throws IOException {
 
+        // Character streams
+
         String filename = "src\\streams\\output\\text.txt";
 
         CharArrayWriter charArrayWriter = new CharArrayWriter();
-        FileWriter card1 = new FileWriter("src\\streams\\output\\text1.txt");
-        FileWriter card2 = new FileWriter("src\\streams\\output\\text2.txt");
+        FileWriter card1 = new FileWriter("src\\streams\\output\\text1.txt", true);
+        FileWriter card2 = new FileWriter("src\\streams\\output\\text2.txt", true);
 
         charArrayWriter.write("Hello this is my Business card");
         charArrayWriter.writeTo(card1);
@@ -19,11 +21,36 @@ public class Output {
 
         char[] arr = charArrayWriter.toCharArray();
 
+        System.out.println(charArrayWriter);
+
         System.out.println(Arrays.toString(arr));
 
         charArrayWriter.close();
         card1.close();
         card2.close();
+
+
+        // Byte Streams
+
+
+        OutputStream output = new FileOutputStream(filename, true);
+
+        byte[] bytes = {65, 66};
+
+        output.write(bytes);
+
+        output.write(bytes, 0, 2);
+
+        output.write(65);
+
+
+
+
+        output.close();
+
+
+
+
 
     }
 

50912b1cd78f3655270b35e5ff07a8d5919b47b0
Tenno641 <ahmedfotoh2003@gmail.com>
2023-08-22 16:49:25 +0300

    Update Main.java

--- src/excutors/Main.java
+++ src/excutors/Main.java
@@ -11,34 +11,22 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         ExecutorService executor1 = Executors.newSingleThreadExecutor();
-        ExecutorService executor2 = Executors.newSingleThreadExecutor();
         sharedData data = new sharedData();
 
         for (int i = 0; i < 10; i++) {
-            synchronized (data) {
-                executor1.submit(() -> {
-                    System.out.println("Start process");
-                    data.dec();
-                    System.out.println("It's done\n");
-                });
-            }
+
+            executor1.submit(() -> {
+
+                System.out.println("Start process");
+                data.inc();
+                System.out.println("It's done\n");
+
+            });
         }
 
         executor1.shutdown();
 
-        for (int i = 0; i < 10; i++) {
-            synchronized (data) {
-                executor2.submit(() -> {
-                    System.out.println("Start process");
-                    data.inc();
-                    System.out.println("It's done\n");
-                });
-            }
-        }
-
-        executor2.shutdown();
-
-        System.out.println(data.num);
+        System.out.println(data.getNum());
 
     }
 
@@ -46,14 +34,10 @@ public class Main {
 
 class sharedData {
 
-    volatile int num = 0;
+    int num = 0;
 
-    public synchronized void inc() {
+    public void inc() {
         num++;
-    }
-
-    public synchronized void dec() {
-        num--;
     }
 
     public int getNum() {
